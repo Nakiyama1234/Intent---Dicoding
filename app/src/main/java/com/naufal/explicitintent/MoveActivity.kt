b4a5b68023e7af1e95a0e1296c432d78a60ae3f8
Nakiyama123 <naufalidn2009@gmail.com>
2023-11-09 11:03:07 +0700

    Dicoding - Codelab Explicit Intent

--- app/src/main/java/com/naufal/explicitintent/MoveActivity.kt
+++ app/src/main/java/com/naufal/explicitintent/MoveActivity.kt
@@ -2,10 +2,13 @@ package com.naufal.explicitintent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MoveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move)
+
+        contentResolver
     }
 }
